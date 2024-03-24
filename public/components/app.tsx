894a852cbe6b6b2530391f1a1a9b8b3521d04c51
Liam <crosszerot@gmail.com>
2024-03-24 12:55:30 -0400

    complete version 0.1

--- public/components/app.tsx
+++ public/components/app.tsx
@@ -13,11 +13,15 @@ import {
   EuiPageContentHeader,
   EuiPageHeader,
   EuiTitle,
-  EuiText,
   EuiFilePicker,
+  EuiPopover,
+  EuiForm,
+  EuiFormRow,
+  EuiFieldText,
   EuiFlexGroup,
   EuiFlexItem,
   EuiSpacer,
+  EuiDescribedFormGroup,
 } from '@elastic/eui';
 
 import { CoreStart } from '../../../../src/core/public';
@@ -39,9 +43,90 @@ export const AutomatedIrpApp = ({
   navigation,
 }: AutomatedIrpAppDeps) => {
 
-  let BearerKey = ""
+  const [shuffleIp, setShuffleIp] = useState("");
+  const [bearerKey, setBearerKey] = useState("");
+
+  const [shuffleIpTemp, setShuffleIpTemp] = useState("");
+  const [bearerKeyTemp, setBearerKeyTemp] = useState("");
 
   const [files, setFiles] = useState({});
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const onButtonClick = () => {
+    setIsPopoverOpen(!isPopoverOpen);
+  };
+
+  const closePopover = () => {
+    setIsPopoverOpen(false);
+  };
+
+  const button = (
+    <EuiButton
+      iconSide="right"
+      fill
+      iconType="arrowDown"
+      onClick={onButtonClick}
+    >
+      Изменить данные Shuffle
+    </EuiButton>
+  );
+  
+  const onClickSave = () => {    
+
+    const ip = String(shuffleIpTemp);
+    const key = String(bearerKeyTemp);
+    var re_key = new RegExp('^[[0-9]|a-f]{8}-([[0-9]|a-f]{4}-){3}[[0-9]|a-f]{12}$');
+    var re_ip = new RegExp('(^([0-9]{1,3}\.){3}[0-9]{1,3}$)|(^[a-zA-Z0-9][a-zA-Z0-9-_]{0,61}[a-zA-Z0-9]{0,1}\.([a-zA-Z]{1,6}|[a-zA-Z0-9-]{1,30}\.[a-zA-Z]{2,3})$)');
+
+    if (key != '')
+      if (re_key.test(key)){
+        setBearerKey(key)
+        notifications.toasts.addSuccess(
+        i18n.translate('automatedIrp.dataUpload', {
+          defaultMessage: 'Bearer ключ обновлен: ' + key,
+        }))}
+      else
+        notifications.toasts.addDanger(
+        i18n.translate('automatedIrp.dataDontUpload', {
+          defaultMessage: 'Ошибка валидации Bearer ключа: ' + key,
+        }))
+
+    if (ip != '')
+      if (re_ip.test(ip)){
+        setShuffleIp(ip)
+        notifications.toasts.addSuccess(
+        i18n.translate('automatedIrp.dataUpload', {
+          defaultMessage: 'IP обновлен: ' + ip,
+        }))}
+      else
+        notifications.toasts.addDanger(
+        i18n.translate('automatedIrp.dataDontUpload', {
+          defaultMessage: 'Ошибка валидации ip: ' + ip,
+        }))
+
+  }
+
+  const formSample = (
+    <EuiForm component="form">
+      <EuiFlexGroup>
+        <EuiFlexItem grow={false} style={{ width: 250 }}>
+          <EuiFormRow label="IP Shuffle">
+            <EuiFieldText id="automatedIrp.shuffleip" onChange={(e) => setShuffleIpTemp(e.target.value)} icon="ip" placeholder={ String(shuffleIp) }/>
+          </EuiFormRow>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiFormRow label="Ключ API" style={{ width: 300 }}>
+            <EuiFieldText id="automatedIrp.bearerkey" onChange={(e) => setBearerKeyTemp(e.target.value)} icon="tokenKey" placeholder={ String(bearerKey) }/>
+          </EuiFormRow>
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiFormRow hasEmptyLabelSpace>
+            <EuiButton onClick={() => onClickSave()}>Сохранить</EuiButton>
+          </EuiFormRow>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiForm>
+  );
 
   const onClickHandler = (files: any) => {    
     setFiles(files.length > 0 ? Array.from(files) : []);
@@ -54,16 +139,16 @@ export const AutomatedIrpApp = ({
     );
   };
 
-  const onClickButton = (files: any, BearerKey: string) => {    
+  const onClickButton = (files: any, bearerKey: any, shuffleIP: any) => {    
 
     for(const file of files){
       const form = new FormData();
       form.append('shuffle_file', file, file.name);
 
-      fetch('https://shuffler.io/api/v1/files/file/upload', {
+      fetch('http://' + String(shuffleIP) + '/api/v1/files/file/upload', {
         method: 'POST',
         headers: {
-          'Authorization': 'Bearer ' + BearerKey
+          'Authorization': 'Bearer ' + String(bearerKey)
         },
         body: form
       })
@@ -82,11 +167,8 @@ export const AutomatedIrpApp = ({
           }))
       })
     }
-    // Use the core http service to make a response to the server API.
   };
 
-  // Render the application DOM.
-  // Note that `navigation.ui.TopNavMenu` is a stateful component exported on the `navigation` plugin's start contract.
   return (
     <Router basename={basename}>
       <I18nProvider>
@@ -121,16 +203,31 @@ export const AutomatedIrpApp = ({
                   </EuiTitle>
                 </EuiPageContentHeader>
                 <EuiPageContentBody>
-                  <EuiText>
-                    <p>
-                      <FormattedMessage
-                        id="automatedIrp.content"
-                        defaultMessage="Загрузите коллекции артефактов для анализа ниже"
-                      />
-                    </p>
-                    <EuiHorizontalRule />
+                  <div>
+                          <EuiDescribedFormGroup
+                      title={<h3>Поддерживает различные форматы контейнеров: E01, VMDK, QCoW и др</h3>}
+                      description={
+                        <>
+                          <p>
+                          Загрузите коллекции артефактов для анализа ниже
+                          </p>
+                        </>
+                      }
+                    >
+                        <EuiPopover
+                        button={button}
+                        isOpen={isPopoverOpen}
+                        closePopover={closePopover}
+                      >
+                        <div style={{ width: 700 }}>{formSample}</div>
+                      </EuiPopover> 
+                    </EuiDescribedFormGroup>
+
                     
-                  </EuiText>
+                  </div>
+                  <EuiHorizontalRule />
+
+                  <div>
                   <EuiFlexGroup>
                     <EuiFlexItem>
                         <EuiFilePicker
@@ -139,14 +236,17 @@ export const AutomatedIrpApp = ({
                           initialPromptText="Выберите или перетащите несколько файлов"
                           onChange={onClickHandler}
                           display='large'
-                          aria-label="Use aria labels when no actual label is in use"
+                          fullWidth
                         />
                       <EuiSpacer />
                     </EuiFlexItem>
                   </EuiFlexGroup>
-                    <EuiButton type="submit" size="s" onClick={() => onClickButton(files, BearerKey)}>
+                  </div>
+                  <div>
+                    <EuiButton type="submit" size="s" onClick={() => onClickButton(files, bearerKey, shuffleIp)}>
                       <FormattedMessage id="automatedIrp.buttonText" defaultMessage="Analyze" />
                     </EuiButton>
+                  </div>
                 </EuiPageContentBody>
               </EuiPageContent>
             </EuiPageBody>
